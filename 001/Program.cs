d33bd780e2a0c640a5f98892090cc1014181b03e
Denis <beznikind@gmail.com>
2023-01-27 22:36:55 +0300

    first commit

---
+++ 001/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+void PrintArray(int[] array)
+{
+    int len = array.Length;
+    int index = 0;
+    while (index < len)
+    {
+        array [index] = new Random().Next(0,1);
+        index = index + 1 ;
+    }
+}
+int[] array = new int[8];
+PrintArray(array);
+Console.WriteLine(string.Join("," , array ));
---
+++ 002/Program.cs
@@ -0,0 +1,18 @@
+﻿// Определить, присутствует ли в заданном массиве, некоторое число
+int[] array = {1,22,34,5,6,7,8,9,0,10};
+
+void MetodRandom(int[] array, int find )
+{
+    int num = 0;
+ int len = array.Length;
+ for (int index = 0; index < len; index = index + 1 )
+ {
+    if (array[index] == find )
+    Console.WriteLine("Число "+ $"{ find}" + " есть в массиве"   );
+ 
+   
+
+ }
+
+}
+MetodRandom(array,find : 1 );
